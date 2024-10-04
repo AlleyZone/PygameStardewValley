0210adb96a5668fb2f4dad4687968210a802adaa
Alley <llzeynolla@vk.com>
2024-10-04 21:02:51 +0500

    Basic player

--- player.py
+++ player.py
@@ -9,3 +9,25 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.Surface((64, 30))
         self.image.fill('green')
         self.rect = self.image.get_rect(center=pos)
+
+        self.direction = pygame.math.Vector2()
+        
+    def input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_UP]:
+            self.direction.y = -1
+        elif keys[pygame.K_DOWN]:
+            self.direction.y = 1
+        else:
+            self.direction.y = 0
+
+        if keys[pygame.K_RIGHT]:
+            self.direction.x = 1
+        elif keys[pygame.K_LEFT]:
+            self.direction.x = -1
+        else:
+            self.direction.y = 0
+
+    def update(self, dt):
+        self.input()
